9cae213477c45f229d13aff1d53e20801dc275ad
Benoit Blanchard <benoit.l.blanchard@gmail.com>
2024-03-06 10:42:21 +0100

    controller booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,40 @@
 class BookingsController < ApplicationController
+
+
+  def new
+    @station = Station.find(params[:station_id])
+    @booking = @station.bookings.new
+  end
+
+  def create
+    @station = Station.find(params[:station_id])
+    @booking = @station.bookings.new(booking_params)
+    @booking.user_id = current_user.id
+    if @booking.save
+      redirect_to bookings_path(@bookings)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @booking = Booking.new
+  end
+
+  def edit; end
+
+  def update
+    @station.update(station_params)
+    redirect_to station_path(@station)
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:quantity)
+  end
 end
