3f3e71527e5f99c53e5164fee2fce1053c31c871
Honorine Faure gh api ...honorinefaure@gmail.com <honorinefaure@gmail.com>
2024-03-06 15:54:34 +0100

    index bookings done, scss to be completed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,11 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    # Pour récupérer les véhicules et les stations de l'utilisateur actuel
+    user_vehicles = current_user.vehicles
+    user_stations = current_user.stations
+
+    # Pour afficher seulement les bookings reliés aux véhicules et aux stations de l'utilisateur actuel
+    @bookings = Booking.where(vehicle_id: user_vehicles.ids).or(Booking.where(station_id: user_stations.ids))  end
 end
