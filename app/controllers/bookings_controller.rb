d7e591d1fb1057251a227766fc14ed4149bd071d
Honorine Faure gh api ...honorinefaure@gmail.com <honorinefaure@gmail.com>
2024-03-06 18:29:04 +0100

    bookings page fixed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,10 +5,10 @@ class BookingsController < ApplicationController
   def index
     # Pour récupérer les véhicules et les stations de l'utilisateur actuel
     user_vehicles = current_user.vehicles
-    user_stations = current_user.stations
+    user_station = current_user.station
 
     # Pour afficher seulement les bookings reliés aux véhicules et aux stations de l'utilisateur actuel
-    @bookings = Booking.where(vehicle_id: user_vehicles.ids).or(Booking.where(station_id: user_stations.ids))  
+    @bookings = Booking.where(vehicle_id: user_vehicles.ids).or(Booking.where(station_id: user_station.id))
   end
 
   before_action :set_booking, only: [:show, :edit, :update, :destroy]
