faff6a21d8e59666b13754ae0b7599ed0a25c64d
Benoit Blanchard <benoit.l.blanchard@gmail.com>
2024-03-12 09:20:49 +0100

    push du matin

--- app/channels/tchat_mate_channel.rb
+++ app/channels/tchat_mate_channel.rb
@@ -1,16 +1,9 @@
 class TchatMateChannel < ApplicationCable::Channel
   def subscribed
-    tchatmate = TchatMate.find(params[:id])
-    stream_from "tchat_mate_channel"
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-
-  def speak(data)
-    Message.create! content: data['message'], user: current_user
+    booking = Booking.find(params[:id])
+    stream_for booking
 
   end
+
 
 end
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -5,6 +5,6 @@ import { Autocomplete } from 'stimulus-autocomplete'
 const application = Application.start()
 application.register('autocomplete', Autocomplete)
 // Configure Stimulus development experience
-application.debug = false;
+// application.debug = false;
 
 export { application };
