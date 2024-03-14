83db649cbac1c9f7aa1a9edbb171b4932cdb4da7
Benoit Blanchard <benoit.l.blanchard@gmail.com>
2024-03-14 12:47:34 +0100

    putain j'ai enfin fini

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,13 +2,12 @@ class BookingsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_booking, only: [:show, :update, :destroy]
 
-
   def index
     # Pour récupérer les véhicules et la station de l'utilisateur actuel
     user_vehicles = current_user.vehicles
     user_station = current_user.station
     user_vehicles_ids = Vehicle.where(user_id: params[:user_id]).pluck(:id)
-    station_bookings = current_user.station.bookings.where.not(status: 'en_attente_de_soumission') if current_user.station.present?
+    station_bookings = current_user.station.bookings.where.not(status: 'en_attente_de_confirmation') if current_user.station.present?
     # Pour afficher seulement les bookings reliés aux véhicules et aux stations de l'utilisateur actuel
     if current_user.station
       @bookings = Booking.where(vehicle_id: user_vehicles.ids).or(Booking.where(station_id: user_station.id)).order('created_at DESC')
@@ -26,7 +25,7 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @booking.station_id = params[:station_id]
 
-    @booking.status = 'en_attente_de_soumission'
+    @booking.status = 'en_attente_de_confirmation'
 
     if @booking.save
       redirect_to bookings_path
@@ -44,7 +43,8 @@ class BookingsController < ApplicationController
     @is_owner = (current_user == @booking.vehicle.user)
     check_and_return(@booking)
     case @booking.status
-      when "en_attente_de_confirmation" then submit_offer
+      when "en_attente_de_confirmation" then confirm_offer
+      when "en_attente_de_soumission" then submit_offer
       when "en_attente_de_validation" then accept_offer
       when "en_attente_de_paiement" then validate_payment
       when "en_attente_de_charge" then start_charge
@@ -54,6 +54,8 @@ class BookingsController < ApplicationController
       @booking, {
         owner: render_to_string(partial: "bookings/action_choice", locals: { booking: @booking, is_owner: true } ),
         customer: render_to_string(partial: "bookings/action_choice", locals: { booking: @booking, is_owner: false } ),
+        # messages: render_to_string(partial: "messages/message", locals: { booking: @booking }, formats: [:html])
+
       }
     )
   end
@@ -68,12 +70,16 @@ class BookingsController < ApplicationController
 
 
   def check_and_return(booking)
-    if booking.status == "en_attente_de_soumission"
+    if booking.status == "en_attente_de_confirmation"
       booking.update(booking_params)
       booking.initier_offre!
       redirect_to booking_path(booking)
       return
     end
+  end
+
+  def confirm_offer
+    @booking.confirmer_offre!
   end
 
   def submit_offer
